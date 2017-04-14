de69856bc9ebbd48ba473828ccd4f8bd6b63915d
qtds8810 <1961156518@qq.com>
2017-04-14 15:09:20 +0800

    小修改

--- 06_NavigationControllers/ViewController5.swift
+++ 06_NavigationControllers/ViewController5.swift
@@ -27,16 +27,29 @@ class ViewController5: UIViewController {
     
     /// 移除导航控制器中指定的（单个、多个连续|不连续）控制器
     private func removeChildControllers() {
-        guard let tmpControllers = navigationController?.viewControllers else { return }
-        var controllers = tmpControllers
         
-        // 从导航控制器的堆栈中连续移除多个维护的控制器，推荐用这种方法，注意数组越界的问题，因此从最大的开始遍历，逐级往最小的移除
-        for i in (1 ..< (controllers.count-1)).reversed() {
-            controllers.remove(at: i)
-            navigationController?.viewControllers = controllers
+        // 移除导航控制器中指定的多个连续控制器，方法一：
+        if let tmpControllers = navigationController?.viewControllers {
+            var controllers = tmpControllers
+            // 从导航控制器的堆栈中连续移除多个维护的控制器，推荐用这种方法，注意数组越界的问题，因此从最大的开始遍历，逐级往最小的移除
+            for i in (1 ..< (controllers.count-1)).reversed() {
+                controllers.remove(at: i)
+                navigationController?.viewControllers = controllers
+            }
         }
         
-//        // 从导航控制器的堆栈中移除指定的控制器，推荐用这种方法
+//        // 移除导航控制器中指定的多个连续控制器，方法二：
+//        guard let tmpControllers = navigationController?.viewControllers else { return }
+//        var controllers = tmpControllers
+//        
+//        // 从导航控制器的堆栈中连续移除多个维护的控制器，推荐用这种方法，注意数组越界的问题，因此从最大的开始遍历，逐级往最小的移除
+//        for i in (1 ..< (controllers.count-1)).reversed() {
+//            controllers.remove(at: i)
+//            navigationController?.viewControllers = controllers
+//        }
+        
+        
+//        // 移除导航控制器中指定的多个不连续控制器，推荐用这种方法
 //        for (i, controller) in (controllers.enumerated()).reversed() {
 //            if controller.isKind(of: ViewController4.classForCoder()) {
 //                controllers.remove(at: i)
